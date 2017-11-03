e2fdeb384e2eca6a5aa125dd68a52010e68fc4c5
中妻洋幸 <Hiroyuki_Nakatsuma@122x220x150x68.ap122.ftth.ucom.ne.jp>
2017-11-03 20:54:02 +0900

    画像アップロードボタンを一つに変更

--- src/main/resources/static/js/album/input.js
+++ src/main/resources/static/js/album/input.js
@@ -1,5 +1,22 @@
 
 // 画像を複数同時にアップロードする
+function imageUpload() {
+    const roomId = Number($('body').data('roomid'));
+    const imageInputForm = $('#imageInputForm')[0];
+    const formData = new FormData(imageInputForm);
+
+    $.ajax({
+        type: 'POST',
+        url: `/album/input/${roomId}`,
+        data: formData,
+        processData: false,
+        contentType: false
+    }).done((data) => {
+        location.href = `/album/${roomId}`;
+    }).fail((data) => {
+        $('.album').append('<div class="alert alert-danger">画像の登録に失敗しました。</div>');
+    });
+}
 
 $(() => {
     // CSRFトークンの設定
@@ -9,25 +26,14 @@ $(() => {
         xhr.setRequestHeader(header, token);
     });
 
-    $('#js-image-select').on('click', () => {
+    $('#js-image-upload').on('click', () => {
         $('#files').click();
     });
 
-    $('#js-image-upload').on('click', function() {
-        const roomId = Number($('body').data('roomid'));
-        const imageInputForm = $('#imageInputForm')[0];
-        const formData = new FormData(imageInputForm);
-
-        $.ajax({
-            type: 'POST',
-            url: `/album/input/${roomId}`,
-            data: formData,
-            processData: false,
-            contentType: false
-        }).done((data) => {
-            location.href = `/album/${roomId}`;
-        }).fail((data) => {
-            $('.album').append('<div class="alert alert-danger">画像の登録に失敗しました。</div>');
-        });
+    $('#files').on('change', function () {
+        var file = this.files[0];
+        if(file != null) {
+            imageUpload();
+        }
     });
 });
