46252dcf5241bd6362bcfecedbf74a807d8a0a43
中妻洋幸 <Hiroyuki_Nakatsuma@122x220x150x68.ap122.ftth.ucom.ne.jp>
2017-11-05 18:15:13 +0900

    ルーム作成のバリデーションの修正

--- src/main/java/com/example/ssa/ssa/controller/form/RoomCreateForm.java
+++ src/main/java/com/example/ssa/ssa/controller/form/RoomCreateForm.java
@@ -1,10 +1,12 @@
 package com.example.ssa.ssa.controller.form;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.NotEmpty;
 
 @Data
 public class RoomCreateForm {
-    @NotEmpty
+    @NotEmpty(message = "{room.roomName.notEmpty}")
+    @Length(max = 40, message = "{room.roomName.max}")
     private String roomName;
 }
